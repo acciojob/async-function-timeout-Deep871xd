df6448a77bb07d8c48b243c221cb19e5d0cc13c1
Deep871xd <102525444+Deep871xd@users.noreply.github.com>
2023-07-03 16:12:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-//your JS code here. If required.
+async function showMessageWithDelay(message, delay) {
+  await new Promise(resolve => setTimeout(resolve, delay));
+  return message;
+}
+
+
+
+    const btn = document.getElementById('btn');
+const output = document.getElementById('output');
+
+btn.addEventListener('click', async () => {
+  const textInput = document.getElementById('text').value;
+  const delayInput = document.getElementById('delay').value;
+
+  const message = await showMessageWithDelay(textInput, delayInput);
+  output.textContent = message;
+});
